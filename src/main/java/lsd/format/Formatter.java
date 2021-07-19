965b7f1c2bd437af12157359bf418b823a99fe1b
lukasz.gryzbon <lukasz.gryzbon@integreety.co.uk>
2021-07-19 09:36:49 +0100

    #2 Serialise objects to pretty printed JSON

--- src/main/java/lsd/format/Formatter.java
+++ src/main/java/lsd/format/Formatter.java
@@ -1,10 +1,20 @@
 package lsd.format;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+
 import static lsd.format.json.JsonPrettyPrinter.indentJson;
 import static lsd.format.xml.XmlPrettyPrinter.indentXml;
 
 public class Formatter {
-    public static String indent(final String document) {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String prettyPrint(final String document) {
         return indentJson(document).orElseGet(() -> indentXml(document).orElse(document));
+    }
+
+    @SneakyThrows
+    public static String prettyPrintJson(final Object object) {
+        return indentJson(objectMapper.writeValueAsString(object)).orElseThrow();
     }
 }
--- src/test/java/lsd/format/FormatterShould.java
+++ src/test/java/lsd/format/FormatterShould.java
@@ -1,5 +1,6 @@
 package lsd.format;
 
+import lombok.Value;
 import org.approvaltests.core.Options;
 import org.junit.jupiter.api.Test;
 
@@ -9,28 +10,41 @@ import java.nio.file.Paths;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.Files.readString;
-import static lsd.format.Formatter.indent;
+import static lsd.format.Formatter.prettyPrint;
+import static lsd.format.Formatter.prettyPrintJson;
 import static org.approvaltests.Approvals.verify;
 import static org.approvaltests.Approvals.verifyXml;
 
 class FormatterShould {
 
+    private final Options options = new Options().forFile().withExtension(".json");
+
     @Test
     void formatJson() throws IOException, URISyntaxException {
-        verify(indent(readDocument("/source/source.json")), new Options().forFile().withExtension(".json"));
+        verify(prettyPrint(readDocument("/source/source.json")), options);
     }
 
     @Test
     void formatXml() throws IOException, URISyntaxException {
-        verifyXml(indent(readDocument("/source/source.xml")));
+        verifyXml(prettyPrint(readDocument("/source/source.xml")));
     }
 
     @Test
     void returnOriginalIfNeitherJsonNorXml() throws IOException, URISyntaxException {
-        verify(indent(readDocument("/source/source.txt")));
+        verify(prettyPrint(readDocument("/source/source.txt")));
+    }
+
+    @Test
+    void serialiseObjectToJson() {
+        verify(prettyPrintJson(new ExampleObject(2)), options);
     }
 
     private String readDocument(String fileName) throws IOException, URISyntaxException {
         return readString(Paths.get(getClass().getResource(fileName).toURI()), UTF_8);
+    }
+
+    @Value
+    static class ExampleObject {
+        int value;
     }
 }
