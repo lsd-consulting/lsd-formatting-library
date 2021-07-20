177894ab8771f196d8e1b26b0bf0763d6939bf97
lukasz.gryzbon <lukasz.gryzbon@integreety.co.uk>
2021-07-20 22:56:55 +0100

    Serialise arrays in a single line in JSON

--- src/main/java/lsd/format/json/JsonPrettyPrinter.java
+++ src/main/java/lsd/format/json/JsonPrettyPrinter.java
@@ -1,33 +1,31 @@
 package lsd.format.json;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NoArgsConstructor;
 
+import java.util.Map;
 import java.util.Optional;
 
-import static com.google.gson.JsonParser.parseString;
 import static java.util.Optional.empty;
 import static lombok.AccessLevel.PRIVATE;
+import static lsd.format.json.ObjectMapperCreator.PRETTY_PRINTER;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
 @NoArgsConstructor(access = PRIVATE)
 public class JsonPrettyPrinter {
 
-    private final static Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private final static ObjectMapper objectMapper = ObjectMapperCreator.create();
 
     public static Optional<String> indentJson(final String document) {
         if (isBlank(document)) {
             return empty();
         }
 
-        final JsonElement jsonElement;
         try {
-            jsonElement = parseString(document);
-            return Optional.of(GSON.toJson(jsonElement));
-        } catch (final JsonParseException e) {
+            var map = objectMapper.readValue(document, Map.class);
+            return Optional.of(objectMapper.setDefaultPrettyPrinter(PRETTY_PRINTER).writeValueAsString(map));
+        } catch (final JsonProcessingException e) {
             return empty();
         }
     }
--- src/main/java/lsd/format/json/ObjectMapperCreator.java
+++ src/main/java/lsd/format/json/ObjectMapperCreator.java
@@ -1,14 +1,26 @@
 package lsd.format.json;
 
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 
 public class ObjectMapperCreator {
+    public static final DefaultPrettyPrinter PRETTY_PRINTER = new DefaultPrettyPrinter() {
+        @Override
+        public DefaultPrettyPrinter createInstance() {
+            super._objectFieldValueSeparatorWithSpaces = ": ";
+            return new DefaultPrettyPrinter(this);
+        }
+    };
+
     public static ObjectMapper create() {
+
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         objectMapper.registerModule(new JavaTimeModule());
         objectMapper.configure(WRITE_DATES_AS_TIMESTAMPS, false);
         objectMapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
