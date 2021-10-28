d76098949548d411dcc684dc470d0e124567047a
lukasz.gryzbon <lukasz.gryzbon@integreety.co.uk>
2021-10-28 12:07:46 +0100

    Add parsing JSON to a map

--- src/main/java/lsd/format/json/ObjectMapperCreator.java
+++ src/main/java/lsd/format/json/ObjectMapperCreator.java
@@ -5,12 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.kotlin.KotlinModule;
+import org.jetbrains.annotations.NotNull;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 
 public class ObjectMapperCreator {
     public static final DefaultPrettyPrinter PRETTY_PRINTER = new DefaultPrettyPrinter() {
+        @NotNull
         @Override
         public DefaultPrettyPrinter createInstance() {
             super._objectFieldValueSeparatorWithSpaces = ": ";
@@ -23,6 +26,7 @@ public class ObjectMapperCreator {
         objectMapper.setDefaultPrettyPrinter(PRETTY_PRINTER);
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
         objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new KotlinModule.Builder().build());
         objectMapper.registerModule(customModule());
         objectMapper.configure(WRITE_DATES_AS_TIMESTAMPS, false);
         objectMapper.configure(FAIL_ON_UNKNOWN_PROPERTIES, false);
---
+++ src/test/java/lsd/format/ParserShould.java
@@ -0,0 +1,37 @@
+package lsd.format;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.Files.readString;
+import static java.util.Objects.requireNonNull;
+import static lsd.format.Parser.parse;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class ParserShould {
+
+    @Test
+    void parseJson() throws IOException, URISyntaxException {
+        Map<String, Object> result = parse(readDocument("/source/source.json"));
+
+        assertThat(result, hasEntry("key", List.of("value1", "value2", "value3")));
+    }
+
+    @Test
+    void returnEmptyMapIfNotJson() throws IOException, URISyntaxException {
+        Map<String, Object> result = parse(readDocument("/source/source.txt"));
+
+        assertThat(result, is(anEmptyMap()));
+    }
+
+    private String readDocument(String fileName) throws IOException, URISyntaxException {
+        return readString(Paths.get(requireNonNull(getClass().getResource(fileName)).toURI()), UTF_8);
+    }
+}
