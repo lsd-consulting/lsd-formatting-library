b2477c65b091ad306bd34ef674d34b091a50625b
Lukasz Gryzbon <lgryzbon@ford.com>
2023-08-03 20:36:51 +0100

    fix: Simplify indenting JSON

--- src/main/java/lsd/format/json/JsonPrettyPrinter.kt
+++ src/main/java/lsd/format/json/JsonPrettyPrinter.kt
@@ -1,41 +1,16 @@
 package lsd.format.json
 
 import com.fasterxml.jackson.core.JsonProcessingException
-import com.fasterxml.jackson.databind.exc.MismatchedInputException
 import lsd.format.config.log
-import org.apache.commons.lang3.StringUtils
-import java.util.*
 
 fun indentJson(document: String?): String? {
-    return if (StringUtils.isBlank(document)) {
+    return if (document.isNullOrBlank()) {
         null
     } else try {
-        indentJsonMap(document)
-    } catch (e: MismatchedInputException) {
-        if (e.message!!.contains("JsonToken.START_ARRAY")) {
-            return try {
-                indentJsonList(document)
-            } catch (ex: JsonProcessingException) {
-                log().trace(ex.message)
-                null
-            }
-        }
-        log().error(e.message)
-        null
+        val map = objectMapper.readTree(document)
+        return objectMapper.writeValueAsString(map)
     } catch (e: JsonProcessingException) {
         log().debug(e.message)
         null
     }
-}
-
-@Throws(JsonProcessingException::class)
-private fun indentJsonMap(document: String?): String? {
-    val map: Map<*, *> = objectMapper.readValue(document, MutableMap::class.java)
-    return objectMapper.writeValueAsString(map)
-}
-
-@Throws(JsonProcessingException::class)
-private fun indentJsonList(document: String?): String? {
-    val list: List<*> = objectMapper.readValue(document, MutableList::class.java)
-    return objectMapper.writeValueAsString(list)
 }
